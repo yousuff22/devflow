a74562255f8e747406602510bc818214ae7213e4
yousuff22 <yousuf.sayyed.cse@ghrcemp.raisoni.net>
2025-11-26 18:31:57 +0530

    Added: OAuth with Google Provider

--- components/forms/SocialFormAuth.tsx
+++ components/forms/SocialFormAuth.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
 import { Button } from "../ui/button";
 import { toast } from "sonner";
 import { signIn } from "next-auth/react";
